7eeec3d0bd0de974b090ac6537f8d2f57d47205c
Marie3emeVoie <marie.3emevoie@gmail.com>
2025-08-11 16:54:23 +0200

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  theme: { extend: {} },
   plugins: [],
-}
+};
